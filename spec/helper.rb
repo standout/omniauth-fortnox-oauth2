3a3d99c19597f5c8b362f54a7c59f3fad85e5ed8
Emil Svensson <svenne87@gmail.com>
2021-09-28 22:41:07 +0200

    Deploy gem on release

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,7 +4,6 @@ $LOAD_PATH.unshift File.expand_path(__dir__)
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 require 'rspec'
-require 'rack/test'
 require 'webmock/rspec'
 require 'omniauth'
 require 'omniauth-fortnox-oauth2'
@@ -14,6 +13,5 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
   config.extend OmniAuth::Test::StrategyMacros, type: :strategy
-  config.include Rack::Test::Methods
   config.include WebMock::API
 end
